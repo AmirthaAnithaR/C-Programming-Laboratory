6877341fd0aa4bd4d3da4eee43d667eb7ba3f69e
AmirthaAnithaR <anithaami1208@gmail.com>
2024-10-06 22:06:01 +0530

    Create equalnos.c

---
+++ equalnos.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    scanf("%d %d",&a,&b);
+    if (a==b){
+        printf("They are equal");
+    }
+    
+    else{
+        printf("They are not equal");
+    }
+}
+
+
