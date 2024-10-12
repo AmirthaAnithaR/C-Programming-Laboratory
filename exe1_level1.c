be10c77c23d19afc242140b26c365ce802680817
AmirthaAnithaR <anithaami1208@gmail.com>
2024-10-12 17:13:06 +0530

    Create exe1_level1.c

---
+++ exe1_level1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    float a;
+    scanf("%f",&a);
+    float x=a/2.0;
+    float b=pow(x,2);
+    float area=3.14*b;
+    printf("%.2f",area);
+    return 0;
+    
+}
+
+
+
